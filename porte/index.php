970e17f1814f6b6a1b9989f7473e731cc160aeca
Ricardo Teixeira <47228282+kika2001@users.noreply.github.com>
2023-02-27 19:27:35 +0000

    commit on main

--- porte/index.php
+++ porte/index.php
@@ -1,6 +1,3 @@
 <?php
-   header("Access-Control-Allow-Credentials": "true",
-   "Access-Control-Allow-Headers": "Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time",
-   "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-   "Access-Control-Allow-Origin": "*"); 
+    header("Access-Control-Allow-Origin: *");
  ?>  
