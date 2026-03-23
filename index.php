c24e7a33971223b6222005b6b54ba877651c1c5c
Ricardo Teixeira <47228282+kika2001@users.noreply.github.com>
2026-03-23 14:31:07 +0000

    Sample website base

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-   header("Access-Control-Allow-Credentials": "true",
-   "Access-Control-Allow-Headers": "Accept, X-Access-Token, X-Application-Name, X-Request-Sent-Time",
-   "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-   "Access-Control-Allow-Origin": "*"); 
- ?>  
